fc1ce1ccedc28d4972fcf33514d43f8f6011edf5
ant-pearl <carlosalvatici@gmail.com>
2020-08-12 11:09:53 -0500

    Initial Commit

---
+++ Man.py
@@ -0,0 +1,7 @@
+print("that other file is weird")
+
+var = 1
+
+while var < 5:
+    print(var)
+    var++
---
+++ Sup.py
@@ -0,0 +1,8 @@
+print("test")
+bigBoy = 4
+while bigBoy < 5:
+    print(bigBoy)
+    bigBoy++
+
+if bigBoy < 5:
+    print("what the fuck")
